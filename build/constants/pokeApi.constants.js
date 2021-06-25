1a46d7c716b51980ab87ef1a935909e5ff877a00
vishal sharma <vishal.sharma@fluper.in>
2021-06-25 09:00:36 +0530

    update

--- build/constants/pokeApi.constants.js
+++ build/constants/pokeApi.constants.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = void 0;
 exports.PORT = 8001;
-console.log("PORT", exports.PORT);
+console.log("==PORT==", exports.PORT);
 //# sourceMappingURL=pokeApi.constants.js.map
